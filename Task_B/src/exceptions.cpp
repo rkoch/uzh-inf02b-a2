0194ca9baeeed6c791862e0b2a1bfde5badd1e1c
Remo Koch <me@rko.io>
2013-03-03 16:29:17 +0100

    Finish work on Task B

---
+++ Task_B/src/exceptions.cpp
@@ -0,0 +1,20 @@
+//
+//  exceptions.cpp
+//  stack_arraybased
+//
+//  Created by Loc Nguyen on 23.02.12.
+//  Copyright (c) 2012 Universität Zürich. All rights reserved.
+//
+//  Authored by Remo Koch
+//
+
+#include <iostream>
+#include "exceptions.h"
+
+void OverFlow::overflowoutput() {
+	std::cout << "Overflow, stack is full\n";
+}
+
+void UnderFlow::underflowoutput() {
+	std::cout << "Underflow, stack is empty\n";
+}
---
+++ Task_B/src/stack_ab.cpp
@@ -0,0 +1,52 @@
+//
+//  stack_ab.cpp
+//  stack_arraybased
+//
+//  Created by Loc Nguyen on 16.02.12.
+//  Copyright (c) 2012 Universität Zürich. All rights reserved.
+//
+//  Authored by Remo Koch
+//
+
+#include <iostream>
+#include "stack_ab.h"
+
+Stack_ab::Stack_ab() {
+	index = 0;
+}
+
+Stack_ab::~Stack_ab() {
+	// Nothing to delete as data is only held on the stack and gets destroyed upon exiting current scope.
+	// See:
+	// http://stackoverflow.com/questions/2722879/calling-constructors-in-c-without-new
+}
+
+int Stack_ab::size() {
+	return index;
+}
+
+void Stack_ab::push(std::string newelement) {
+	if (index >= StackMax) {
+		throw OverFlow();
+	}
+	data[index++] = newelement;
+}
+
+std::string Stack_ab::pop() {
+	if (index <= 0) {
+		throw UnderFlow();
+	}
+	return data[--index];
+}
+
+std::string Stack_ab::top() {
+	if (index <= 0) {
+		throw UnderFlow();
+	}
+	return data[index - 1];
+}
+
+bool Stack_ab::empty() {
+	return index <= 0;
+}
+
